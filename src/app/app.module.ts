28fae87e05c1400db80847a0b1b596d69aa9ad38
井川 拓信 <h_igawa@ardito.jp>
2017-12-07 18:30:48 +0900

    working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,10 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
 
+import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
 
 
@@ -10,7 +13,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase, 'mafia'),
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
